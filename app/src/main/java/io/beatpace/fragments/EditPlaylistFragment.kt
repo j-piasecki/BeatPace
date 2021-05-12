172a5fc25e0ef35ed77c9f83b06e4098946d8465
Jakub Piasecki <jakubpiasecki67@gmail.com>
2021-05-12 11:58:14 +0200

    Add partial EditPlaylistFragment implementation Add partial PlaylistModifierAdapter implementation

--- app/src/main/java/io/beatpace/fragments/EditPlaylistFragment.kt
+++ app/src/main/java/io/beatpace/fragments/EditPlaylistFragment.kt
@@ -6,25 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.beatpace.R
 import io.beatpace.api.ViewModel
+import io.beatpace.fragments.adapters.PlaylistModifierAdapter
 
 class EditPlaylistFragment : Fragment() {
 
     private val viewModel: ViewModel by activityViewModels()
+    private lateinit var adapter: PlaylistModifierAdapter
 
-    val args: EditPlaylistFragmentArgs by navArgs()
+    private val args: EditPlaylistFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        adapter = PlaylistModifierAdapter(
+            viewModel.getPlaylistManager().getPlaylistById(args.playlistId)!!,
+            viewModel.getPlaylistManager(),
+            viewModel.getSongs()
+        )
 
         return inflater.inflate(R.layout.fragment_edit_playlist, container, false)
     }
 
-    private fun onAddSongClick() {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private fun onDeletePlaylistClick() {}
+        val recyclerView = view.findViewById<RecyclerView>(R.id.edit_playlist_songs)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+
+        view.findViewById<FloatingActionButton>(R.id.edit_playlist_add_song).setOnClickListener(this::onAddSongClick)
+    }
+
+    private fun onAddSongClick(view: View) {
+        findNavController().navigate(EditPlaylistFragmentDirections.actionEditPlaylistFragmentToNewSongFragment(args.playlistId))
+    }
+
+    private fun onDeletePlaylistClick(playlistId: Int) {
+
+    }
 }
