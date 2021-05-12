c67e9ce653df2a0215080eeb2820b3c75f1862a3
Jakub Piasecki <jakubpiasecki67@gmail.com>
2021-05-12 12:30:01 +0200

    Add partial NewSongFragment implementation Add partial SongPickerAdapter implementation

--- app/src/main/java/io/beatpace/fragments/NewSongFragment.kt
+++ app/src/main/java/io/beatpace/fragments/NewSongFragment.kt
@@ -7,20 +7,45 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.beatpace.R
 import io.beatpace.api.ViewModel
+import io.beatpace.fragments.adapters.SongPickerAdapter
 
 class NewSongFragment : Fragment() {
 
     private val viewModel: ViewModel by activityViewModels()
+    private lateinit var adapter: SongPickerAdapter
 
-    val args: NewSongFragmentArgs by navArgs()
+    private val args: NewSongFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        adapter = SongPickerAdapter(
+            viewModel.getPlaylistManager().getPlaylistById(args.playlistId)!!,
+            viewModel.getPlaylistManager(),
+            viewModel.getSongs()
+        )
 
         return inflater.inflate(R.layout.fragment_new_song, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.new_song_input)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+
+        view.findViewById<FloatingActionButton>(R.id.new_song_confirm).setOnClickListener(this::onConfirmClick)
+    }
+
+    private fun onConfirmClick(view: View) {
+        val selectedSongs = adapter.getSelectedSongs()
     }
 }
