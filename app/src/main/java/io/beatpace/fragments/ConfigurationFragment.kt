3ff5a75eba366a5f3567bf85ecd5d1b24f1eeb6f
Jakub Piasecki <jakubpiasecki67@gmail.com>
2021-05-12 10:40:26 +0200

    Add path to change selected path

--- app/src/main/java/io/beatpace/fragments/ConfigurationFragment.kt
+++ app/src/main/java/io/beatpace/fragments/ConfigurationFragment.kt
@@ -4,26 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.beatpace.R
+import io.beatpace.api.ViewModel
+import io.beatpace.api.data.structures.Playlist
 
 class ConfigurationFragment : Fragment() {
+
+    private val viewModel: ViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_configuration, container, false)
     }
 
-    private fun onPaceChangeClick() {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private fun onPlaylistChangeClick() {}
+        view.findViewById<Button>(R.id.configuration_set_playlist).setOnClickListener(this::onPlaylistChangeClick)
+        view.findViewById<Button>(R.id.configuration_set_speed).setOnClickListener(this::onPaceChangeClick)
+        view.findViewById<FloatingActionButton>(R.id.configuration_start_button).setOnClickListener(this::onStartClick)
 
-    private fun onStartClick() {}
+        setPaceText(viewModel.getDataConfig().getSelectedPace())
+        setPlaylistText(viewModel.getDataConfig().getSelectedPlaylistId())
+    }
 
-    private fun onPaceText() {}
+    private fun onPaceChangeClick(view: View) {
+        findNavController().navigate(ConfigurationFragmentDirections.actionConfigurationFragmentToPaceSelectFragment())
+    }
 
-    private fun setPlaylistText() {}
+    private fun onPlaylistChangeClick(view: View) {
+        findNavController().navigate(ConfigurationFragmentDirections.actionConfigurationFragmentToPlaylistSelectFragment())
+    }
+
+    private fun onStartClick(view: View) {
+        if (viewModel.getDataConfig().getSelectedPlaylistId() >= 0) {
+            findNavController().navigate(ConfigurationFragmentDirections.actionConfigurationFragmentToMonitoringFragment())
+        } else {
+            Toast.makeText(context, requireContext().getString(R.string.no_playlist_selected), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setPaceText(pace: Double) {
+        view?.findViewById<TextView>(R.id.configuration_current_speed)?.text = pace.toString()
+    }
+
+    private fun setPlaylistText(playlistId: Int) {
+        view?.findViewById<TextView>(R.id.configuration_current_playlist)?.text =
+            if (playlistId < 0)
+                requireContext().getString(R.string.no_playlist_selected)
+            else
+                viewModel.getPlaylistManager().getPlaylistById(playlistId)?.name
+    }
 }
