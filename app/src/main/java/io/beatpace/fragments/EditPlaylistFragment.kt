e1de2c4487a6ef7b0d669ff470b783b3137bf587
Jakub Piasecki <jakubpiasecki67@gmail.com>
2021-05-12 11:30:38 +0200

    Add partial ManagePlaylistsFragment implementation Add navigation args to EditPlaylistFragment and NewSongFragment

--- app/src/main/java/io/beatpace/fragments/EditPlaylistFragment.kt
+++ app/src/main/java/io/beatpace/fragments/EditPlaylistFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import io.beatpace.R
+import io.beatpace.api.ViewModel
 
 class EditPlaylistFragment : Fragment() {
+
+    private val viewModel: ViewModel by activityViewModels()
+
+    val args: EditPlaylistFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/io/beatpace/fragments/NewSongFragment.kt
+++ app/src/main/java/io/beatpace/fragments/NewSongFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import io.beatpace.R
+import io.beatpace.api.ViewModel
 
 class NewSongFragment : Fragment() {
+
+    private val viewModel: ViewModel by activityViewModels()
+
+    val args: NewSongFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +23,4 @@ class NewSongFragment : Fragment() {
 
         return inflater.inflate(R.layout.fragment_new_song, container, false)
     }
-
-    //TODO change to NewSongFragment
 }
