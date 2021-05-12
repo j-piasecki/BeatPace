d7ae7008ee58c8b905c9ea79d88bb5dc2e954fd3
Jakub Piasecki <jakubpiasecki67@gmail.com>
2021-05-12 12:30:01 +0200

    Add partial NewSongFragment implementation Add partial SongPickerAdapter implementation

--- app/src/main/java/io/beatpace/fragments/adapters/SongPickerAdapter.kt
+++ app/src/main/java/io/beatpace/fragments/adapters/SongPickerAdapter.kt
@@ -1,4 +1,69 @@
 package io.beatpace.fragments.adapters
 
-class SongPickerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import io.beatpace.R
+import io.beatpace.api.PlaylistManager
+import io.beatpace.api.data.structures.Playlist
+
+class SongPickerAdapter(
+    private val playlist: Playlist,
+    private val playlistManager: PlaylistManager,
+    private val songs: Map<Long, String>
+) : ListAdapter<Long, SongPickerAdapter.ViewHolder>(object : DiffUtil.ItemCallback<Long>() {
+    override fun areItemsTheSame(oldItem: Long, newItem: Long): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Long, newItem: Long): Boolean {
+        return oldItem == newItem
+    }
+}){
+
+    private val selectedSongs = ArrayList<Long>()
+
+    init {
+        submitList(songs.keys.toList())
+
+        for (i in 0 until playlist.getSize())
+            selectedSongs.add(playlist.getSong(i))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_song_pick, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    fun getSelectedSongs(): List<Long> {
+        return selectedSongs
+    }
+
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(position: Int) {
+            val songId = getItem(position)
+
+            view.findViewById<TextView>(R.id.row_song_pick_name).apply {
+                text = songs[songId]
+            }
+
+            view.isSelected = selectedSongs.contains(songId)
+            view.setOnClickListener {
+                if (selectedSongs.contains(songId))
+                    selectedSongs.remove(songId)
+                else
+                    selectedSongs.add(songId)
+
+                notifyItemChanged(position)
+            }
+        }
+    }
 }
