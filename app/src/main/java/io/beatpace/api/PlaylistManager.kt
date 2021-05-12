8889d98e693d480d577b0d880412d7cf310bcec4
lunalight54 <paweladamczuk2000@gmail.com>
2021-05-12 18:10:52 +0200

    implemented Playlist Class and PlaylistManager Class

--- app/src/main/java/io/beatpace/api/PlaylistManager.kt
+++ app/src/main/java/io/beatpace/api/PlaylistManager.kt
@@ -2,27 +2,70 @@ package io.beatpace.api
 
 import io.beatpace.api.data.structures.Playlist
 import io.beatpace.database.DAO
+import io.beatpace.database.PlaylistContent
+import io.beatpace.database.PlaylistEntity
 
 class PlaylistManager(private val dao: DAO) {
 
     private lateinit var playlists: MutableMap<Int, Playlist>
 
-    fun getAllPlaylists(): List<Playlist> {
-        TODO("Not yet implemented")
+    init{ // get all playlists and save them into our playlists map
+
+        var entityList = dao.getPlaylists() // a temporary PlaylistEntityList
+
+        for ( i in entityList.indices){ // creating and adding all our playlists into our map from the database
+
+            var songList = ArrayList<Long>()// a list of all the songs in current playlist
+
+            var contentList = dao.getAllSongs(entityList[i].playlistId!!)
+
+            for (j in contentList.indices){ // convert playlistContent list into a list of songId's (Long)
+                songList.add(contentList[j].songId)
+            }
+
+            var newPlaylist = Playlist( entityList[i].title, entityList[i].playlistId!!, songList) // Creating our playlist so we can add it into our map
+
+            playlists.put(newPlaylist.Id, newPlaylist) // adding the playlist into the map
+        }
     }
 
-    fun getPlaylistById(playlistId: Int): Playlist {
-        TODO("Not yet implemented")
+    fun getAllPlaylists(): List<Playlist> { // turn out playlists map into a list
+        val playlistList: List<Playlist> = playlists.values.toList()
+        return  playlistList
     }
 
-    fun deletePlaylistById(playlistId: Int) {}
+    fun getPlaylistById(playlistId: Int): Playlist? {
+        return playlists[playlistId]
+    }
+
+    fun deletePlaylistById(playlistId: Int) {
+        playlists.remove(playlistId)
+        dao.deletePlaylist(playlistId)
+    }
 
     fun createPlaylist(playlistName: String): Int {
-        TODO("Not yet implemented")
+        var newPlaylistId = dao.createNewPlaylist(playlistName).toInt()
+        var newPlaylist = Playlist( playlistName, newPlaylistId, mutableListOf()) // w ten sposób?
+        playlists.put(newPlaylistId, newPlaylist)
+        return newPlaylistId
     }
 
-    fun addSongToPlaylist(playlistId: Int, songId: Long) {}
+    fun addSongToPlaylist(playlistId: Int, songId: Long) {
+        var current: Playlist? = playlists[playlistId]
+        if (current == null)
+            return
+        current.addSongId(songId)
+        var newContent = PlaylistContent(playlistId, songId)
+        dao.insertNewSong(newContent)
+    }
 
-    fun removeSongFromPlaylist(playlistId: Int, songId: Long) {}
+    fun removeSongFromPlaylist(playlistId: Int, songId: Long) {
+        var current: Playlist? = playlists[playlistId]
+        if (current == null)
+            return
+        current.deleteSongId(songId)
+        var selectedContent = PlaylistContent(playlistId, songId)
+        dao.removeSong(selectedContent)
+    }
 
 }
--- app/src/main/java/io/beatpace/api/data/structures/Playlist.kt
+++ app/src/main/java/io/beatpace/api/data/structures/Playlist.kt
@@ -1,17 +1,23 @@
 package io.beatpace.api.data.structures
 
-data class Playlist(var name: String, var Id: Int, private val songList: List<Long>) {
+data class Playlist(var name: String, var Id: Int, private val songList: MutableList<Long>) { // MutableList lista która można zmieniać
 
-    fun addSongId(songId: Long) {}
+    fun addSongId(songId: Long) {
+        if (!containsSong(songId)) // sprawdza czy piosenka nie jest już w playliscie
+            songList.add(songId)
+    }
 
-    fun deleteSongId(songId: Long) {}
+    fun deleteSongId(songId: Long) {
+        if (containsSong(songId))
+            songList.remove(songId)
+    }
 
     fun getSong(songPosition: Int): Long {
-        TODO("Not yet implemented")
+        return songList[songPosition]
     }
 
     fun getSize(): Int {
-        TODO("Not yet implemented")
+        return songList.size
     }
 
     fun containsSong(songId: Long): Boolean {
