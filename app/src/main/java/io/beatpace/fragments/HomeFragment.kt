22f1e63e66401e98e57aceba8daff1b0163c609a
Jakub Piasecki <jakubpiasecki67@gmail.com>
2021-05-12 10:40:26 +0200

    Add path to change selected path

--- app/src/main/java/io/beatpace/fragments/HomeFragment.kt
+++ app/src/main/java/io/beatpace/fragments/HomeFragment.kt
@@ -4,22 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import io.beatpace.R
+import io.beatpace.api.ViewModel
 
 
 class HomeFragment : Fragment() {
+
+    private val viewModel: ViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    private fun onStartClick() {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private fun onPlaylistsClick() {}
+        view.findViewById<Button>(R.id.home_start_button).setOnClickListener(this::onStartClick)
+        view.findViewById<Button>(R.id.home_playlists_button).setOnClickListener(this::onPlaylistsClick)
+    }
+
+    private fun onStartClick(view: View) {
+        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToConfigurationFragment())
+    }
+
+    private fun onPlaylistsClick(view: View) {
+        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToManagePlaylistsFragment())
+    }
 }
