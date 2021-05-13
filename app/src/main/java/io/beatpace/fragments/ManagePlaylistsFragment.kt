fe08f5f3adfe9160faaa22777405cd06602170e8
Jakub Piasecki <jakubpiasecki67@gmail.com>
2021-05-13 08:24:29 +0200

    Add new playlist creation

--- app/src/main/java/io/beatpace/fragments/ManagePlaylistsFragment.kt
+++ app/src/main/java/io/beatpace/fragments/ManagePlaylistsFragment.kt
@@ -1,10 +1,14 @@
 package io.beatpace.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -44,6 +48,29 @@ class ManagePlaylistsFragment : Fragment() {
     }
 
     private fun onAddPlaylistClick(view: View) {
-        TODO("Tworzenie nowych playlist")
+        val view = layoutInflater.inflate(R.layout.dialog_new_playlist, null)
+        val input = view.findViewById<EditText>(R.id.dialog_new_playlist_name)
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(view)
+            .setCancelable(true)
+            .setTitle(requireContext().getString(R.string.create_new_playlist))
+            .setNegativeButton(android.R.string.cancel) { dialog, _ ->
+                dialog.cancel()
+            }
+            .setPositiveButton(android.R.string.ok) { dialog, _ ->
+                val name = input.text.toString().trim()
+
+                if (name.isNotEmpty()) {
+                    val id = viewModel.getPlaylistManager().createPlaylist(name)
+
+                    findNavController().navigate(ManagePlaylistsFragmentDirections.actionManagePlaylistsFragmentToEditPlaylistFragment(id))
+                } else {
+                    Toast.makeText(context, requireContext().getString(R.string.no_playlist_selected), Toast.LENGTH_SHORT).show()
+                }
+            }
+            .create()
+
+        dialog.show()
     }
 }
