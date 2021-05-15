fae134afa2a33f765f260c3c7ed67329cc2288ef
MultiDestroy <pprochot16@gmail.com>
2021-05-15 14:15:42 +0200

    Create implementation for pace monitoring
    -All parameters are hardcoded for those implementations
    -Add implementation for MonitoringFragment.kt
    -Add implementation for MonitoringService.kt
    -Add implementation for MusicController.kt

--- app/src/main/java/io/beatpace/api/MusicController.kt
+++ app/src/main/java/io/beatpace/api/MusicController.kt
@@ -1,8 +1,9 @@
 package io.beatpace.api
 
-import com.google.android.exoplayer2.ExoPlayer
+import android.content.ContentUris
+import android.provider.MediaStore
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.ui.StyledPlayerView
 import io.beatpace.api.data.structures.Playlist
 import java.util.*
@@ -13,8 +14,8 @@ class MusicController(private val exoPlayer: SimpleExoPlayer) {
 
     fun startPlaying(playlist: Playlist, pace: Double) {
         this.targetPace = pace
-        val songs = getMediaSourceList(playlist)
-        exoPlayer.addMediaSources(songs)
+        val mediaItemList = getMediaItemList(playlist)
+        exoPlayer.addMediaItems(mediaItemList)
 
         exoPlayer.prepare()
         exoPlayer.play()
@@ -30,17 +31,35 @@ class MusicController(private val exoPlayer: SimpleExoPlayer) {
     }
 
     fun onPaceUpdate(pace: Double) {
-        /*
-            Some operations that returns target volume
-         */
-
-
+        exoPlayer.volume = (pace / targetPace).toFloat()
     }
 
-    private fun getMediaSourceList(playlist: Playlist) : List<MediaSource> {
-        val list = LinkedList<Long>()
-        playlist.getSize()
-        return LinkedList()
+    private fun getMediaItemList(playlist: Playlist): List<MediaItem> {
+        val mediaItemList = ArrayList<MediaItem>()
+
+        for (i in 0 until playlist.getSize()) {
+            val id = playlist.getSong(i)
+            val item = MediaItem.fromUri(
+                ContentUris.appendId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI.buildUpon(), id
+                ).build()
+            )
+
+            mediaItemList.add(item)
+        }
+
+        //TODO remove
+        val a =
+            MediaItem.fromUri("file:///android_asset/Game of Thrones S8 Official Soundtrack A Song of Ice and Fire - Ramin Djawadi WaterTower.mp3");
+
+        return LinkedList(listOf(a))
     }
+
+    fun getCurrentSong(): MediaItem? {
+        return exoPlayer.currentMediaItem
+    }
+
+    /* ----------- */
+
 
 }
--- app/src/main/java/io/beatpace/fragments/MonitoringFragment.kt
+++ app/src/main/java/io/beatpace/fragments/MonitoringFragment.kt
@@ -1,24 +1,91 @@
 package io.beatpace.fragments
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import io.beatpace.R
+import io.beatpace.api.MonitoringService
+import io.beatpace.api.data.structures.Playlist
+import java.util.*
 
 class MonitoringFragment : Fragment() {
+
+    private var serviceBound = false
+    private lateinit var service: MonitoringService
+    private val serviceConnection = MonitoringServiceConnection()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_monitoring, container, false)
     }
 
-    private fun showCurrentPace(someDouble: Double) {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private fun onFinishClick() {}
+        startService()
+        activity?.bindService(Intent(context, MonitoringService::class.java), serviceConnection, 0)
+        view.findViewById<Button>(R.id.monitoring_finish_button).setOnClickListener(this::onFinishClick)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if (serviceBound) {
+            activity?.unbindService(serviceConnection)
+        }
+    }
+
+    private fun onFinishClick(view: View) {
+        if (serviceBound) {
+            service.stopMonitoring()
+            service.stopSelf()
+        }
+        findNavController().navigate(MonitoringFragmentDirections.actionMonitoringFragmentToEndFragment())
+    }
+
+    private fun startService() {
+        val servInt = Intent(context, MonitoringService::class.java)
+        activity?.startService(servInt)
+    }
+
+    fun startMonitoring() {
+        val playerView = view?.findViewById<StyledPlayerView>(R.id.player_view)
+        service.attachPlayerToView(playerView!!)
+        service.setPaceDisplayListener(this::showCurrentPace)
+
+        val songs = ArrayList<Long>(listOf(1))
+        val playlist = Playlist("New playlist", 1, songs)
+        val pace = 10.0
+        service.startMonitoring(playlist, pace)
+    }
+
+    private fun showCurrentPace(someDouble: Double) {
+        TODO()
+    }
+
+    inner class MonitoringServiceConnection: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            serviceBound = true
+
+            val binder = service as MonitoringService.LocalBinder
+
+            this@MonitoringFragment.service = binder.getService()
+            this@MonitoringFragment.startMonitoring()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            serviceBound = false
+        }
+    }
 
 }
